7da6d50f9b4a6b180856f220f1daec9e726a9112
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-10-26 17:04:38 +0200

    aaaa

--- apps/newstrapi-cms/config/server.ts
+++ apps/newstrapi-cms/config/server.ts
@@ -1,7 +1,6 @@
 export default ({ env }: any) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://cms-load-balancer-1540236931.eu-south-1.elb.amazonaws.com/',
   app: {
     keys: env.array('APP_KEYS'),
   },
--- apps/strapi-cms/config/admin.ts
+++ apps/strapi-cms/config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }: any) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','dfee1c43fb8d2e506ad627'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','dfee1c43fb8d2e506ad627'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT','dfee1c43fb8d2e506ad627'),
     },
   },
   autoOpen: false,
