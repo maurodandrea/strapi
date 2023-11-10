a9b26f2e08ae3101ec1e90385c5e57d05d993f96
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-11-10 12:43:23 +0100

    aaa

--- packages/eslint-config/eslint-recommended.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: [
-    // Load common config
-    "./eslint-config.js",
-    // Load pagopa eslint config
-    "@pagopa/eslint-config/recommended",
-  ],
-}
