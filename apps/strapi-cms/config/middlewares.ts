7bad75d0711af39e885f3167f0acea76546f34fd
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-11-16 09:57:56 +0100

    aaaa

--- apps/strapi-cms/config/middlewares.ts
+++ apps/strapi-cms/config/middlewares.ts
@@ -1,6 +1,31 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'cms-medialibrary-9938.s3.eu-south-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'cms-medialibrary-9938.s3.eu-south-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::query',
   'strapi::body',
