df2a940565d1551a29d041ab9ba15b40f2ba7527
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2023-11-16 16:23:07 +0100

    aaa

--- apps/strapi-cms/config/plugins.ts
+++ apps/strapi-cms/config/plugins.ts
@@ -1,8 +1,11 @@
+import AWS from 'aws-sdk';
+
 export default ({ env }: any) => ({
     upload: {
       config: {
         provider: 'aws-s3',
         providerOptions: {
+          credentials: new AWS.ECSCredentials(),
           params: {
             Bucket: env('AWS_BUCKET'),
           },
