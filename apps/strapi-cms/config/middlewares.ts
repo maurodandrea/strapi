5f31e9df9a020de7b68a9675f02aa8fb6feaca42
Simone Salsi <simone.salsi@dgsspa.com>
2023-11-17 11:17:37 +0100

    Added middleware

--- apps/strapi-cms/config/middlewares.ts
+++ apps/strapi-cms/config/middlewares.ts
@@ -1,6 +1,30 @@
 export default [
-  'strapi::errors',
-  'strapi::security',
+  'strapi::errors',{
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'cms-medialibrary-6373.s3.eu-south-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'cms-medialibrary-6373.s3.eu-south-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::query',
   'strapi::body',
